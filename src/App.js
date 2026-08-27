804558639de71c44a7da664d111f515d97e59a16
RodManzella <manzellateixeira@gmail.com>
2026-08-26 21:49:29 -0300

    new adjustments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+
+const sound = new Audio('/dry-fart.mp3');
 
 function App() {
   return (
@@ -13,10 +16,14 @@ function App() {
 
 
 function ImageContainer(){
+  const [loadingAnimation, setLoadingAnimation] = useState(0);
 
   async function handleFile(event){
     const file = event.target.files[0];
     if (!file) return;
+
+    setLoadingAnimation((animation) => animation + 1);
+    event.target.value = '';
     console.log(file);
 
     const bucketUrl = `https://${process.env.REACT_APP_S3_NAME}.s3.sa-east-1.amazonaws.com/${encodeURIComponent(file.name)}`;
@@ -41,7 +48,11 @@ function ImageContainer(){
   return(
     <div className="image-container">
       <Letreiro></Letreiro>
-      <img src="/images/bro-is-captured.jpg" className="cute-cat" alt="cat-funny-pic" />
+      <div className="image-wrapper">
+        <img src="/images/bro-is-captured.jpg" className="cute-cat glow" alt="cat-funny-pic" />
+
+      </div>
+      
 
       <label htmlFor="file-upload" className="upload-button">
         Upload
@@ -51,14 +62,54 @@ function ImageContainer(){
        id="file-upload"
        type="file" 
        onChange={handleFile}
+       
        />
+      <LoadingBar
+        key={loadingAnimation}
+        shouldAnimate={loadingAnimation > 0}
+      ></LoadingBar>
+       
     </div>
+    
+
+  
   );
 }
 
 function Letreiro(){
   return(
     <h1 className="Letreiro">Dig's Upload </h1>
+  );
+}
+
+function LoadingBar({ shouldAnimate }){
+  const [isAnimating, setIsAnimating] = useState(shouldAnimate);
+  const resetTimer = useRef(null);
+
+  useEffect(() => {
+    return () => clearTimeout(resetTimer.current);
+  }, []);
+
+  function handleAnimationEnd(){
+    sound.currentTime = 0;
+    sound.play().catch((error) => {
+      console.error('Error playing sound:', error);
+    });
+
+    resetTimer.current = setTimeout(() => {
+      setIsAnimating(false);
+    }, 1000);
+  }
+
+  return(
+    <div className="loading-bar-container">
+      <div 
+        className={`loading-bar ${isAnimating ? 'play-animation' : ''}`}
+        onAnimationEnd={handleAnimationEnd}  
+        >
+        
+      </div>
+    </div>
   );
 }
 
